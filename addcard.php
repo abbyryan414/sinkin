b4ee501098e9b4e1bd9f0b1bed265fb13075f5b9
iammarco <marcokwoncc0822@gmail.com>
2020-12-25 11:38:18 +0800

    fixed ajax bug

--- addcard.php
+++ addcard.php
@@ -45,9 +45,9 @@
 
     <h1>Add a card:</h1>
     <form method="post">
-        <input name="deck_or_card_title" placeholder="card name:" type="text">
-        <input name="card_info" placeholder="card info:" type="text">
-        <select name='chosen_deck'>
+        <input id="deck_or_card_title" name="deck_or_card_title" placeholder="card name:" type="text">
+        <input id="card_info" name="card_info" placeholder="card info:" type="text">
+        <select id="chosen_deck" name='chosen_deck'>
             <?php
               while($row=$result->fetch_assoc()){
                 $deck_select_name = $row['deck_or_card_title'];
@@ -73,76 +73,35 @@
 
 
         </select>
-        <button name="confirm-btn">confirm</button>
-        <?php
-         
-          if(array_key_exists('confirm-btn', $_POST)) { 
-
-            //get local time by inputing GMT value
-            $local_time = getLocalTime($_SESSION['gmt_int']);
-            echo "Local Time: ".$local_time."<br>";
-          
-            $deck_or_card_title=$_POST['deck_or_card_title'];
-            $card_info=$_POST['card_info'];
-
-            //gets the deck the user selected (or the default deck if he didn't select one)
-            $chosen_deck=$_POST['chosen_deck'];
-
-            //store the deck the user selected to $_SESSION['selected_deck'], so that the default deck will be the deck the user has just selected
-            // NOTE: will have to refresh in order for the default deck option to change
-            $_SESSION['selected_deck'] = $chosen_deck;
-            echo $_SESSION['selected_deck'];
         
-            //select possible repeating card name
-            $value = "SELECT * FROM users_cards WHERE username= '$login_username' AND deck_or_card_title = '$deck_or_card_title' AND currentpath = '$current_path$chosen_deck/'";
-            $result = $conn->query($value);
-            //check for names including /
-            if (preg_match("(/)","$deck_or_card_title")){
-            
-                echo "Card name cannot include / ";
-                
-                }elseif (strlen($deck_or_card_title) > 0 && strlen(trim($deck_or_card_title)) == 0){
-                    //check for space of name
-                    echo "Card name cannot be blank";
-                }elseif (preg_match("(^[NULL]{0}$)","$deck_or_card_title")){
-                    //check for blank of name
-                    echo "Card name cannot be blank";
-                }elseif($result->num_rows > 0){
-                    //check for same card
-                    echo "Card name cannot be the same as an existing card";
-                }else{
-             
-                //get the path of the deck the user has chosen, 
-                //store it in $chosen_deck_path
-                $value = "SELECT * FROM users_cards WHERE username= '$login_username' AND deck_or_card_title = '$chosen_deck' AND is_card='0'";
-                $result = $conn->query($value);
-                while($row=$result->fetch_assoc()){
-                  $chosen_deck_path = $row['currentpath'];
-                }
-            
-                //insert data to table
-                $sql = "INSERT INTO users_cards (username,currentpath,is_card,deck_or_card_title,card_info,created_date,study_date,reps)
-                VALUES ('$login_username','$chosen_deck_path$chosen_deck/',TRUE,'$deck_or_card_title','$card_info','$local_time','$local_time','0')";
-            
-                if ($conn->query($sql) === TRUE) {
-                echo "<br>". "New card added";
-                //header("Location:index.php");
-                } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-
-                }
-                header("Refresh:0");
-            }
-                
-          }
-      
-
-      ?>
+        
+       
     </form>
     <form action="index.php">
-                        <button>return to home page</button>
+      <button>return to home page</button>
     </form>
-    
+    <button onclick="confirm()">confirm</button>
+    <script>
+    function confirm() {
+      var deck_or_card_title = document.getElementById('deck_or_card_title').value;
+      var card_info = document.getElementById('card_info').value;
+      var chosen_deck = $("#chosen_deck :selected").val();
+      
+      $.ajax({
+        url: 'phpFiles/ajax_add_card.php',
+        method: 'POST',
+        dataType: 'text',
+        data: {
+          deck_or_card_title: deck_or_card_title,
+          card_info: card_info,
+          chosen_deck: chosen_deck
+        }               
+      }).done(function(returnedData){
+          alert(returnedData);
+          location.reload();
+      })
+    }
+  </script>
 </body>
 </html>
 
