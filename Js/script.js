4bf4ffc779c460a2720e188de187ffe40ccabae7
iammarco <marcokwoncc0822@gmail.com>
2021-01-01 17:25:34 +0800

    bugged fixed

--- Js/script.js
+++ Js/script.js
@@ -7,11 +7,11 @@ function WordSearch(){
     //gets the word user inputted. There must be a better way to do this...
     "method": "GET",
     "headers": {
-    "x-rapidapi-key": "9adf941a45mshca905293069e03bp180a5ajsn652a5848be6e",
-    //Remember to conceal the api key!! Dont let the pirates get it!
-    "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
-}
-})
+      "x-rapidapi-key": "9adf941a45mshca905293069e03bp180a5ajsn652a5848be6e",
+      //Remember to conceal the api key!! Dont let the pirates get it!
+      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+    }
+  })
 //This is where crap hits the fan. GOAL: to display received api data on html. Format: list or table.
 
 //METHOD 1
@@ -19,14 +19,21 @@ function WordSearch(){
     //transforms api response to json
     .then((data)=>{
     let bruh = Object.values(data);
-    console.log(bruh)
+    console.log(bruh);
+    var x = "";
     for(i in data.results){
-        x += "<h1>"+ bruh.results[i].definition + "</h1>";
+        // x += "<h1>"+ bruh.results[i].definition + "</h1>";
+        //x = "<h1>"+ bruh[1][i].definition + "</h1>";
+        x += bruh[1][i].definition + "<br>";
+        //console.log(x);
+        
+        //console.log(bruh[1][1].definition);
         //loop thru the results tab and shows the definitions
         //ERROR: Uncaught (in promise) ReferenceError: x is not defined
         //Do not be fooled. X is defined. JS spits a reference error when it isn't returning something explicitly. Refer to the internet.
     }
-    console.log(x)
+    document.getElementById("definition_area").innerHTML = x;
+    
     //console display all definitions
 })
 
