80b8001b4166b432805fec1eba86ec33e7c91f95
iammarco <marcokwoncc0822@gmail.com>
2021-07-04 21:34:09 +0800

    added dictionary filter options

--- dictionary.php
+++ dictionary.php
@@ -5,15 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dictionary!</title>
     
-    <script src="script.js"></script>
+    <script src="Js/script.js"></script>
 </head>
 <body>
     <div id="output"></div>
     <form name="SearchforWord">
+    </form>
+
     <label for="wordstosearch">Search a word!:</label><br>
     <input type="text" id="searchword" name="searchword"><br>
-    <input type="button" value="Submit" onclick="WordSearch()">
-    </form>
+    <button onclick="WordSearch()">Submit</button>
+    Show Definitions:
+    <input type="checkbox" id="Show Definitions" class="check">
+    Show Examples:
+    <input type="checkbox" id="Show Example" class="check">
+    Show Synonyms:
+    <input type="checkbox" id="Show Synonyms" class="check">
+    Show Antonyms:
+    <input type="checkbox" id="Show Antonyms" class="check">
+    <h5 id="definition_area"></h5>
+    <ol id="example_area"></ol>
+    <ol id="definition_list"></ol>
 
 </body>
 </html>
